f2f2a015278ddc89eaab08cd789f35f9f0e02787
Pablo Camacho <pablocamacho92@gmail.com>
2023-06-17 21:06:25 +0200

    actualizo archivo wsgi

--- src/byte_force/wsgi.py
+++ src/byte_force/wsgi.py
@@ -9,8 +9,18 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
+import sys
+
 from django.core.wsgi import get_wsgi_application
 
+from pathlib import Path
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'byte_force.settings')
+
+path_home = str(Path(__file__).parents[1])
+if path_home not in sys.path:
+    sys.path.append(path_home)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'byte_force.settings'
 
 application = get_wsgi_application()
