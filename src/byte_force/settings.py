016ccd23431ac8c2755b57fb3485f56c5d395161
Pablo Camacho <pablocamacho92@gmail.com>
2023-08-12 16:57:18 +0200

    ON LINE

--- src/byte_force/settings.py
+++ src/byte_force/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+%8(a%@z7_s)tplerdjls@j*znnv4ev12&gk@@dl)ocm@&+5u6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-#ALLOWED_HOSTS = ['www.byte-force.com.ar','byte-force.com.ar']
-ALLOWED_HOSTS= []
+ALLOWED_HOSTS = ['www.byte-force.com.ar','byte-force.com.ar']
+#ALLOWED_HOSTS= []
 
 # Application definition
 
